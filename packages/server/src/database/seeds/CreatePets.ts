7d45d56d65d7eea46cda5058e0664664b4c3981e
Sourodeep Chatterjee <sourodeep224@outlook.com>
2021-02-14 17:56:32 +0530

    Sync dependencies between monorepo

--- packages/server/src/database/seeds/CreatePets.ts
+++ packages/server/src/database/seeds/CreatePets.ts
@@ -7,7 +7,7 @@ import { User } from "../../../src/api/models/User";
 export class CreatePets implements Seeder {
 	public async run(factory: Factory, connection: Connection): Promise<void> {
 		const em = connection.createEntityManager();
-		await times(10, async (n) => {
+		await times(10, async () => {
 			const pet = await factory(Pet)().create();
 			const user = await factory(User)().make();
 			user.pets = [pet];
